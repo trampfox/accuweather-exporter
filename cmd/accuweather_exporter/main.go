ebb2975d5e507670190b2c8217dae18d3021e6d9
Davide Monfrecola <davide.monfrecola@adevinta.com>
2020-05-25 22:51:35 +0200

    feat: add kingpin library to manage app flags

--- cmd/accuweather_exporter/main.go
+++ cmd/accuweather_exporter/main.go
@@ -1,23 +1,26 @@
 package cmd
 
 import (
-	"flag"
 	"log"
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/trampfox/accuweather-exporter/collector"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+var (
+	app    = kingpin.New("accuweather-exporter", "Prometheus Exporter for AccuWeather API").Author("Davide Monfrecola")
+	addr   = app.Flag("listen-address", "The address to listen on for HTTP requests.").Envar("AE_LISTEN_ADDRESS").Default(":9095").String()
+	apiKey = app.Flag("api-key", "The API key for Accuweather API requests.").Envar("AE_API_KEY").Required().String()
+	// TODO add language and unit (metric or imperial)
+)
 
 func Execute() {
-	flag.Parse()
-
 	// Create a new instance of the AccuweatherCollector and
 	// register it with the prometheus client
-	accuweatherCollector := collector.NewAccuweatherCollector()
+	accuweatherCollector := collector.NewAccuweatherCollector(*apiKey)
 	prometheus.MustRegister(accuweatherCollector)
 
 	http.Handle("/metrics", promhttp.Handler())
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/prometheus/client_golang v0.9.3
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
