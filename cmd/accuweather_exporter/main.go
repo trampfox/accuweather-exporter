52a5f417bdbe46963e09e4dfbfeb0fa131621686
Davide Monfrecola <davide.monfrecola@adevinta.com>
2020-06-06 19:39:42 +0200

    chore: rename location key flag

--- cmd/accuweather_exporter/main.go
+++ cmd/accuweather_exporter/main.go
@@ -15,7 +15,7 @@ var (
 	app         = kingpin.New("accuweather-exporter", "Prometheus Exporter for AccuWeather API").Author("Davide Monfrecola")
 	addr        = app.Flag("listen-address", "The address to listen on for HTTP requests.").Envar("AE_LISTEN_ADDRESS").Default(":9095").String()
 	apiKey      = app.Flag("api-key", "The API key for Accuweather API requests.").Envar("AE_API_KEY").Required().String()
-	locationKey = app.Flag("city-id", "The location key of the city for which you want to retrieve current conditions data").Envar("AE_CITY_ID").Default("214753").String()
+	locationKey = app.Flag("location-key", "The location key of the city for which you want to retrieve current conditions data").Envar("AE_LOCATION_KEY").Default("214753").String()
 	location    = app.Flag("location", "The location for which you want to retrieve current conditions data").HintOptions("Turin, IT").Envar("AE_LOCATION").String()
 	// TODO add language and unit (metric or imperial)
 )
